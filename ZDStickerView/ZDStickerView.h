3bf33a20e8d7f1df6f198ec8d890bcaee2756098
Sebastian Dwornik <sebastian.dwornik@scenedoc.com>
2014-11-30 11:09:26 -0500

    Fixes/Changes:
    - Modern Objective-C'ified  (like removing @synthesize)
    - longPress gesture is ON by default
    - fixed: stickerViewDidClose: delegate method now gets called correctly ("delete")
    - renamed 'delegate' to 'stickerViewDelegate', so it's more readable and better defined
    - also made 'stickerViewDelegate' a 'weak' property
    - added -(BOOL)isEditingHandlesHidden;
    - Added functionality that when 'isEditingHandlesHidden == YES' then it also prevents the user from moving the view
    - Updated the DEMO app to reflect some of these changes.
    - Uncrustified the code.  (just my own personal style)

--- ZDStickerView/ZDStickerView.h
+++ ZDStickerView/ZDStickerView.h
@@ -1,12 +1,14 @@
 //
-//  ZDStickerView.h
+// ZDStickerView.h
 //
-//  Created by Seonghyun Kim on 5/29/13.
-//  Copyright (c) 2013 scipi. All rights reserved.
+// Created by Seonghyun Kim on 5/29/13.
+// Copyright (c) 2013 scipi. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "SPGripViewBorderView.h"
+
+
 
 typedef enum {
     ZDSTICKERVIEW_BUTTON_NULL,
@@ -23,15 +25,15 @@ typedef enum {
     SPGripViewBorderView *borderView;
 }
 
-@property (assign, nonatomic) UIView *contentView;
-@property (nonatomic) BOOL preventsPositionOutsideSuperview; //default = YES
-@property (nonatomic) BOOL preventsResizing; //default = NO
-@property (nonatomic) BOOL preventsDeleting; //default = NO
-@property (nonatomic) BOOL preventsCustomButton; //default = YES
+@property (nonatomic, strong) UIView *contentView;
+@property (nonatomic) BOOL preventsPositionOutsideSuperview; // default = YES
+@property (nonatomic) BOOL preventsResizing; // default = NO
+@property (nonatomic) BOOL preventsDeleting; // default = NO
+@property (nonatomic) BOOL preventsCustomButton; // default = YES
 @property (nonatomic) CGFloat minWidth;
 @property (nonatomic) CGFloat minHeight;
 
-@property (strong, nonatomic) id <ZDStickerViewDelegate> delegate;
+@property (weak, nonatomic) id <ZDStickerViewDelegate> stickerViewDelegate;
 
 - (void)hideDelHandle;
 - (void)showDelHandle;
@@ -40,6 +42,7 @@ typedef enum {
 - (void)showCustomHandle;
 - (void)hideCustomHandle;
 - (void)setButton:(ZDSTICKERVIEW_BUTTONS)type image:(UIImage*)image;
+- (BOOL)isEditingHandlesHidden;
 
 @end
 
@@ -50,10 +53,6 @@ typedef enum {
 - (void)stickerViewDidEndEditing:(ZDStickerView *)sticker;
 - (void)stickerViewDidCancelEditing:(ZDStickerView *)sticker;
 - (void)stickerViewDidClose:(ZDStickerView *)sticker;
-#ifdef ZDSTICKERVIEW_LONGPRESS
 - (void)stickerViewDidLongPressed:(ZDStickerView *)sticker;
-#endif
 - (void)stickerViewDidCustomButtonTap:(ZDStickerView *)sticker;
 @end
-
-
--- ZDStickerViewApp/ZDStickerViewApp/ViewController.m
+++ ZDStickerViewApp/ZDStickerViewApp/ViewController.m
@@ -1,13 +1,16 @@
 //
-//  ViewController.m
-//  ZDStickerViewApp
+// ViewController.m
+// ZDStickerViewApp
 //
-//  Created by zedoul on 5/29/13.
-//  Copyright (c) 2013 scipi. All rights reserved.
+// Created by zedoul on 5/29/13.
+// Copyright (c) 2013 scipi. All rights reserved.
 //
 
 #import "ViewController.h"
 #import "ZDStickerView.h"
+
+
+
 
 @interface ViewController () <ZDStickerViewDelegate>
 
@@ -18,35 +21,37 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-	// Do any additional setup after loading the view.
+
+    // Do any additional setup after loading the view.
     UIImageView *imageView1 = [[UIImageView alloc]
-                              initWithImage:[UIImage imageNamed:@"sampleImage1.jpg"]];
-    
+                               initWithImage:[UIImage imageNamed:@"sampleImage1.jpg"]];
+
     UIImageView *imageView2 = [[UIImageView alloc]
                                initWithImage:[UIImage imageNamed:@"sampleImage2.png"]];
     imageView2.backgroundColor = [UIColor clearColor];
     CGRect gripFrame1 = CGRectMake(50, 50, 140, 140);
-    
-    UIView* contentView = [[UIView alloc] initWithFrame:gripFrame1];
+
+    UIView*contentView = [[UIView alloc] initWithFrame:gripFrame1];
     [contentView setBackgroundColor:[UIColor blackColor]];
     [contentView addSubview:imageView1];
     [contentView addSubview:imageView2];
-    
+
+
     ZDStickerView *userResizableView1 = [[ZDStickerView alloc] initWithFrame:gripFrame1];
     userResizableView1.tag = 0;
-    userResizableView1.delegate = self;
-    userResizableView1.contentView = contentView;//contentView;
+    userResizableView1.stickerViewDelegate = self;
+    userResizableView1.contentView = contentView; // contentView;
     userResizableView1.preventsPositionOutsideSuperview = NO;
     [userResizableView1 showEditingHandles];
     [self.view addSubview:userResizableView1];
-    
+
+
     CGRect gripFrame2 = CGRectMake(50, 200, 180, 140);
     UITextView *textView = [[UITextView alloc] initWithFrame:gripFrame2];
     textView.text = @"ZDStickerView is Objective-C module for iOS and offer complete configurability, including movement, resizing, rotation and more, with one finger.";
     ZDStickerView *userResizableView2 = [[ZDStickerView alloc] initWithFrame:gripFrame2];
     userResizableView2.tag = 1;
-    userResizableView2.delegate = self;
+    userResizableView2.stickerViewDelegate = self;
     userResizableView2.contentView = textView;
     userResizableView2.preventsPositionOutsideSuperview = NO;
     userResizableView2.preventsCustomButton = NO;
@@ -57,28 +62,47 @@
     [self.view addSubview:userResizableView2];
 }
 
+
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+
+
 #pragma mark - delegate functions
 
 - (void)stickerViewDidLongPressed:(ZDStickerView *)sticker
 {
-    NSLog(@"%s [%d]",__func__, sticker.tag);
+    NSLog(@"%s [%ld]", __func__, sticker.tag);
+
+    if (sticker.isEditingHandlesHidden)
+    {
+        [sticker showEditingHandles];
+    }
+    else
+    {
+        [sticker hideEditingHandles];
+    }
 }
+
+
 
 - (void)stickerViewDidClose:(ZDStickerView *)sticker
 {
-    NSLog(@"%s [%d]",__func__, sticker.tag);
+    NSLog(@"%s [%ld]", __func__, sticker.tag);
 }
+
+
 
 - (void)stickerViewDidCustomButtonTap:(ZDStickerView *)sticker
 {
-    NSLog(@"%s [%d]",__func__, sticker.tag);
-    [((UITextView*)sticker.contentView) becomeFirstResponder];
+    NSLog(@"%s [%ld]", __func__, sticker.tag);
+    [((UITextView*)sticker.contentView)becomeFirstResponder];
 }
+
+
 
 @end
